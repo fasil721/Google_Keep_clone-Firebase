a1191eb6a07fd5ef22d6ec971a5fb95e605b4a8b
Fasil <fasil17062002@gmail.com>
2022-01-20 14:40:05 +0530

    join page created

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -227,7 +227,7 @@ class HomePage extends StatelessWidget {
                                   return Container();
                                 },
                               );
-                            }),
+                            },),
                       )
                     ],
                   ),
